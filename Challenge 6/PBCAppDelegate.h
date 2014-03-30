d81f374bd878c6d374b7dcac77b60134d1c0af1f
Piero Castro <Cheroky@Piero-Castros-iMac.local>
2014-03-30 15:24:18 -0700

    Initial Commit

---
+++ Challenge 6/PBCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PBCAppDelegate.h
+//  Challenge 6
+//
+//  Created by Piero Castro on 3/30/14.
+//  Copyright (c) 2014 Cheroky Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PBCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Challenge 6/PBCViewController.h
@@ -0,0 +1,13 @@
+//
+//  PBCViewController.h
+//  Challenge 6
+//
+//  Created by Piero Castro on 3/30/14.
+//  Copyright (c) 2014 Cheroky Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PBCViewController : UIViewController
+
+@end
